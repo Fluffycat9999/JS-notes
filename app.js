40e12e9b65a0c8d6c855e9ec141811fc528373ed
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2019-09-09 11:43:23 -0400

    Initial

---
+++ app.js
@@ -0,0 +1,20 @@
+/* //////Primitives
+let numbers = 4 ; //let values can be changed const can't
+let numbers - 5;
+let numbersArrays = [1,2,3,4,5];
+console.log(numbers)
+
+let string = 'string';  //single or double quotes
+let strings = "strings";
+
+let boolean = true; //or false
+
+////////// */
+
+const firstName = 'Rob';
+const job = 'Student';
+const birthYear = '2002';
+let isMarried = false;
+
+/* console.log("Hello my name is " = firstName + "I am a " + job + "and I was born in " + birthYear = "and am I married?" + isMarried); */  //the way it used to be
+console.log(`Hello my name is ${firstName}, I am a ${job}, I was born in ${birthYear}, and am I married? ${isMarried}`)  //how it is now
